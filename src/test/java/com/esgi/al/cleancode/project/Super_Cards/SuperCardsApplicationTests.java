65bfc6a4dd334280f9e1baef1da8ca9da90c082b
Souleman <soulte92@gmail.Com>
2022-11-13 13:05:25 +0100

    Spring project initialized

---
+++ src/test/java/com/esgi/al/cleancode/project/Super_Cards/SuperCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.al.cleancode.project.Super_Cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
