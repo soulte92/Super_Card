940fc4c94b09e65fe35ba1dde24d64c087e7949a
Souleman <soulte92@gmail.Com>
2022-11-13 14:11:34 +0100

    Adding Player model and probability calcutator

---
+++ src/main/java/com/esgi/al/cleancode/project/Super_Cards/Player.java
@@ -0,0 +1,15 @@
+package com.esgi.al.cleancode.project.Super_Cards;
+
+public class Player {
+    private String pseudo;
+    private int nbToken;
+
+    public Player(String pseudo) {
+        this.pseudo = pseudo;
+        this.nbToken = 4;
+    }
+
+    //TODO Ouverture d’un pack de cartes using ProbalityDistribution class
+
+    // TODO Inscription joueur
+}
---
+++ src/main/java/com/esgi/al/cleancode/project/Super_Cards/ProbalityDistribution.java
@@ -0,0 +1,31 @@
+package com.esgi.al.cleancode.project.Super_Cards;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+class ProbalityDistribution<T>{
+    List<Double> probs = new ArrayList<>();
+    List<T> events = new ArrayList<>();
+    double sumProb;
+    Random rand = new Random();
+
+    ProbalityDistribution(Map<T,Double> probs){
+        for(T event : probs.keySet()){
+            sumProb += probs.get(event);
+            events.add(event);
+            this.probs.add(probs.get(event));
+        }
+    }
+
+    public T sample(){
+        T value;
+        double prob = rand.nextDouble()*sumProb;
+        int i;
+        for(i=0; prob>0; i++){
+            prob-= probs.get(i);
+        }
+        return events.get(i-1);
+    }
+}
