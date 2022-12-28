3abcd737848a22aa94bfab8c0056acab12b95228
Souleman <soulte92@gmail.Com>
2022-12-28 09:45:53 +0100

    Add RoundController

--- src/main/java/com/esgi/al/cleancode/project/Super_Cards/adapter/in/RoundController.java
+++ src/main/java/com/esgi/al/cleancode/project/Super_Cards/adapter/in/RoundController.java
@@ -1,4 +1,19 @@
 package com.esgi.al.cleancode.project.Super_Cards.adapter.in;
 
+import com.esgi.al.cleancode.project.Super_Cards.application.service.RoundService;
+import com.esgi.al.cleancode.project.Super_Cards.domain.HeroId;
+import com.esgi.al.cleancode.project.Super_Cards.domain.Round;
+
+import java.util.ArrayList;
+
 public class RoundController {
+    private final RoundService roundService;
+
+    public RoundController(RoundService roundService) {
+        this.roundService = roundService;
+    }
+
+    public ArrayList<Round> getHeroFighterRounds(HeroId heroId){
+        return roundService.getHeroFighterRounds(heroId);
+    }
 }
