60090b7722eb975ff697fb4d54a89c23c44ad32f
Souleman <soulte92@gmail.Com>
2023-02-07 20:02:16 +0100

    Add enhaceCharacteriticsByRarity method in DefaultHeroesPopulationService tests

--- domain/src/test/java/esgi/al/cleancode/project/Super_Cards/domain/functional/service/DefaultHeroesPopulationServiceTest.java
+++ domain/src/test/java/esgi/al/cleancode/project/Super_Cards/domain/functional/service/DefaultHeroesPopulationServiceTest.java
@@ -12,6 +12,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
 import static org.assertj.vavr.api.VavrAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,7 +41,64 @@ class DefaultHeroesPopulationServiceTest {
     }
 
     @Test
-    void enhaceCharacteriticsByRarety() {
+    void should_enhace_hero_caracteritics_by_rarety() {
+        Hero givenCommonHero = Hero.builder()
+                .name("super-boy")
+                .hp(30)
+                .xp(100)
+                .power(20)
+                .armor(5)
+                .level(6)
+                .speciality(Speciality.TANK.label)
+                .rarety(Rarity.COMMON.label).build();
+        Hero actualCommonHero = service.enhaceCharacteriticsByRarity(givenCommonHero);
+        assertEquals(actualCommonHero.hp, 30.0);
+        assertEquals(actualCommonHero.power, 20.0);
+        assertEquals(actualCommonHero.armor, 5.0);
+
+        Hero givenRareHero = Hero.builder()
+                .name("super-boy")
+                .hp(30)
+                .xp(100)
+                .power(20)
+                .armor(5)
+                .level(6)
+                .speciality(Speciality.TANK.label)
+                .rarety(Rarity.RARE.label).build();
+        Hero actualRareHero = service.enhaceCharacteriticsByRarity(givenRareHero);
+        assertEquals(actualRareHero.hp, 33.0);
+        assertEquals(actualRareHero.power, 22.0);
+        assertEquals(actualRareHero.armor, 5.5);
+
+        Hero givenLegendaryHero = Hero.builder()
+                .name("super-boy")
+                .hp(30)
+                .xp(100)
+                .power(20)
+                .armor(5)
+                .level(6)
+                .speciality(Speciality.TANK.label)
+                .rarety(Rarity.LEGENDARY.label).build();
+        Hero actualLegendaryHero = service.enhaceCharacteriticsByRarity(givenLegendaryHero);
+        assertEquals(actualLegendaryHero.hp, 36.0);
+        assertEquals(actualLegendaryHero.power, 24.0);
+        assertEquals(actualLegendaryHero.armor, 6.0);
+    }
+
+    @Test
+    void should_not_enhace_hero_caracteritics_by_not_supported_rarety(){
+        Hero givenHero = Hero.builder()
+                .name("super-boy")
+                .hp(30)
+                .xp(100)
+                .power(20)
+                .armor(5)
+                .level(6)
+                .speciality(Speciality.TANK.label)
+                .rarety("BIGBOSS").build();
+        Assertions.assertThrows(HeroException.class, () -> {
+            service.enhaceCharacteriticsByRarity(givenHero);
+        });
     }
 
     @Test
